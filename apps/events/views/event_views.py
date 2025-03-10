c2b33ff25d15a1953b2dd722e36a17597f70a761
Jordi Homedes <jordihomedes@onabitz.com>
2025-03-10 18:20:39 +0100

    task #65: Created event detail endpoint

--- apps/events/views/event_views.py
+++ apps/events/views/event_views.py
@@ -1,9 +1,15 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from apps.events.models import Event
 from apps.events.serializers import EventSerializer
 
+@api_view(["GET"])
+def get_event_details(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    serializer = EventSerializer(event)
+    return Response(serializer.data)
 
 @api_view(["GET"])
 def get_all_events(request):

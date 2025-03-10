6e41683b8a1f513d4b913a7eb1b6cd44167652f9
Jordi Homedes <jordihomedes@onabitz.com>
2025-03-10 17:43:31 +0100

    task #41: Created endpoint to remove an event from favorites

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -4,5 +4,5 @@ from apps.events.views import event_views, category_views, userevent_views
 urlpatterns = [
     path("", event_views.get_all_events, name="get_all_events"),
     path("/categories", category_views.get_all_categories, name="get_all_categories"),
-    path("/<int:event_id>/favorites", userevent_views.add_to_favorites, name="add_to_favorites"),
+    path("/<int:event_id>/favorites", userevent_views.favorites, name="favorites"),
 ]

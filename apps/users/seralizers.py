b7c10e3cbb7910a832c2370ab01ed0e47867c76b
Jordi Homedes <jordihomedes@onabitz.com>
2025-03-17 16:18:35 +0100

    updating user model WIP

--- apps/users/seralizers.py
+++ apps/users/seralizers.py
@@ -5,4 +5,10 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email"]
+        fields = ["id", "username", "email", "language"]
+
+    def get_language(self, obj):
+        profile = getattr(obj, 'userprofile', None)
+        if profile:
+            return profile.language
+        return None

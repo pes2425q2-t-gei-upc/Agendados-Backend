22dfb237b820293f27d1ae08ec3dd4baad451096
Jordi Homedes <jordihomedes@onabitz.com>
2025-04-14 17:12:42 +0200

    task #128: Configuring websockets server

--- agendadosDjango/asgi.py
+++ agendadosDjango/asgi.py
@@ -3,8 +3,9 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import apps.chat.routing
+from apps import chat
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agendadosDjango.settings')
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(

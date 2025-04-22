56a8a817db87fc6dba27eb708a16c9da3b809890
Jordi Homedes <jordihomedes@onabitz.com>
2025-04-22 19:36:38 +0200

    project working on local environment

--- agendadosDjango/settings.py
+++ agendadosDjango/settings.py
@@ -95,9 +95,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.{}".format(
             os.getenv("DATABASE_ENGINE", "sqlite3")
         ),
-        "NAME": os.getenv("CLOUD_SQL_NAME", "postgres"),
-        "USER": os.getenv("CLOUD_SQL_USERNAME", "postgres"),
-        "PASSWORD": os.getenv("CLOUD_SQL_PASSWORD", "postgres"),
+        "NAME": os.getenv("DATABASE_NAME", "polls"),
+        "USER": os.getenv("DATABASE_USERNAME", "myprojectuser"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "password"),
         "HOST": os.getenv("DATABASE_HOST", "127.0.0.1"),
         "PORT": os.getenv("DATABASE_PORT", 5432),
     }

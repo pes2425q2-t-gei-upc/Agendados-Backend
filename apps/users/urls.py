1f327b7fd3d35f39c611d733870e372daec875ff
Jordi Homedes <jordihomedes@onabitz.com>
2025-04-30 19:04:22 +0200

    task #7: Update password endpoint implemented + swagger documentation

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("login", auth_views.login, name="login"),
     path("signup", auth_views.signup, name="signup"),
     path("test", auth_views.test, name="test"),
+    path("update-password", user_views.update_password, name="update_password"),
     #Friendship urls
     path("friendships", friendship_views.get_friendships, name="friendships"),
     path("friendships/<int:user_id>", friendship_views.send_friend_request, name="send_friend_request"),
--- apps/users/views/user_views.py
+++ apps/users/views/user_views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import update_session_auth_hash
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_201_CREATED
 from django.contrib.auth.models import User
 from django.db.models import Q
 
@@ -28,3 +29,32 @@ def get_users(request):
 
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data, status=HTTP_200_OK)
+
+@api_view(["POST"])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def update_password(request):
+    data = request.data
+    current_password = data.get('current_password', '')
+    new_password = data.get('new_password', '')
+    current_user = request.user
+
+    if not current_password or not new_password:
+        return Response(
+            status=HTTP_400_BAD_REQUEST
+        )
+
+    if not current_user.check_password(current_password):
+        return Response(
+            {"error": "Incorrect current password"},
+            status=HTTP_400_BAD_REQUEST
+        )
+
+    current_user.set_password(new_password)
+    current_user.save()
+
+    update_session_auth_hash(request, current_user)
+
+    return Response(
+        status=HTTP_200_OK
+    )

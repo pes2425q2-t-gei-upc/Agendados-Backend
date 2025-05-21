0991fd4508ad099cd458affbda632edbeeef570b
Jordi Homedes <jordihomedes@onabitz.com>
2025-05-21 17:35:06 +0200

    task #10: created private rooms model + migrations + POST endpoint

--- apps/private_rooms/apps.py
+++ apps/private_rooms/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PrivateRoomsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "private_rooms"
+    name = "apps.private_rooms"
--- apps/private_rooms/views.py
+++ apps/private_rooms/views.py
@@ -1,1 +1,25 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
 
+from apps.private_rooms.models import PrivateRoom
+from apps.private_rooms.serializers import PrivateRoomSerializer
+
+
+@api_view(["POST"])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def create_private_room(request):
+    # Extract data from the request
+    user = request.user
+    data = request.data
+
+    # Create the private room
+    private_room = PrivateRoom.objects.create(
+        code="12345", name=data["name"], admin=user
+    )
+    room_serialized = PrivateRoomSerializer(private_room)
+    # Return the created room details
+    return Response(room_serialized.data, HTTP_201_CREATED)

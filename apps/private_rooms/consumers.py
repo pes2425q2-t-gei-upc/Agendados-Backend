e11e18a7ab61bad26d5f5d3df513c9200e338528
Jordi Homedes <jordihomedes@onabitz.com>
2025-05-22 19:38:10 +0200

    creating/deleting relation user-room when connecting to room

--- apps/private_rooms/consumers.py
+++ apps/private_rooms/consumers.py
@@ -1,4 +1,6 @@
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from apps.private_rooms.models import PrivateRoom
 
 
 class PrivateRoomConsumer(AsyncWebsocketConsumer):
@@ -20,7 +22,15 @@ class PrivateRoomConsumer(AsyncWebsocketConsumer):
         )
         await self.accept()
 
+        #Create the room-user relation
+        room, _ = await sync_to_async(PrivateRoom.objects.get_or_create)(code=self.room_code)
+        await sync_to_async(room.participants.add)(user)
+
     async def disconnect(self, close_code):
+        #Delete the room-user relation
+        room, _ = await sync_to_async(PrivateRoom.objects.get_or_create)(code=self.room_code)
+        await sync_to_async(room.participants.remove)(self.scope['user'])
+
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name

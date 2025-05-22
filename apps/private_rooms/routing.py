1ce3274d68efdd4f93c7ec6f69664ed33acb2f2a
Jordi Homedes <jordihomedes@onabitz.com>
2025-05-22 19:27:52 +0200

    can connect to the websocket room

--- apps/private_rooms/routing.py
+++ apps/private_rooms/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/room/(?P<room_code>\d+)/$', consumers.PrivateRoomConsumer.as_asgi()),
+    re_path(r'^ws/room/(?P<room_code>\w+)/$', consumers.PrivateRoomConsumer.as_asgi()),
 ]

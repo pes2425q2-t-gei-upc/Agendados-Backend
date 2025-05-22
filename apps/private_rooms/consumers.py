8359943f42efc8961952265dcf36d505fd91daee
Jordi Homedes <jordihomedes@onabitz.com>
2025-05-22 19:31:28 +0200

    can connect / disconnect from a room

--- apps/private_rooms/consumers.py
+++ apps/private_rooms/consumers.py
@@ -5,7 +5,23 @@ class PrivateRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         print('Connecting to private room consumer')
         self.room_code = self.scope['url_route']['kwargs']['room_code']
-        user = self.scope['user']
+        self.room_group_name = f'room_{self.room_code}'
 
-        print(self.room_code)
-        print(user)
+        #Get user
+        user = self.scope['user']
+        if not user.is_authenticated:
+            print('User not authenticated, closing connection')
+            await self.close()
+            return
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
